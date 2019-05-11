3526f77ae6bbc260c3a72e50c005c32f3505d8fc
s1ag <s1ag@protonamil.com>
2019-05-11 18:44:17 -0400

    Script is fully functional

--- nessus.sh
+++ nessus.sh
@@ -3,7 +3,6 @@
 # Script for starting and stopping the Nessus vulnerability scanner
 
 arg1=$1
-arg2=$2
 
 raise_error() {
   local error_message="$@"
@@ -33,70 +32,64 @@ stop_nessus() {
 }
 
 start_firefox() {
-    sleep 2s
-    firefox -new-tab "https://$HOSTNAME:8834"
+    # Allows nessusd time to intialize
+    if [[ $arg1 == "-st" || "--start" ]] ; then
+        sleep 3
+    fi 
+    firefox -new-tab "https://${HOSTNAME}:8834"
+    echo "Successfully opened Firefox at https://${HOSTNAME}:8834 "
 }
 
 start_chrome() {
-    sleep 2s
-    google-chrome "https://$HOSTNAME:8834"
+    # Allows nessusd time to intialize
+    if [[ $arg1 == "-st" || "--start" ]] ; then
+    sleep 3
+    fi
+    google-chrome "https://${HOSTNAME}:8834"
+    echo "Successfully opened Chrome at https://${HOSTNAME}:8834"
 }
 
-# If no input
-if [[ -z $arg1 ]] ; then 
+# If no input given
+
+if [[ -z $1 ]] ; then 
     raise_error "No argument given, aborting..."
+    exit 0
 
 # If more than 2 positional arguments
 elif [ "$#" -gt 2 ]; then
     echo "Too many arguments, aborting..."
+    exit 0
+
+# Cannot have the same inputs
+
 
 # If valid number of positional arguments
 else
-    # First argument
-    case $arg1 in
-        -st|--start)
-        start_nessus
-        ;;
+    for argument in "$@"
+        do 
+            case $argument in
+                -st|--start)
+                start_nessus
+                ;;
 
-        -sp|--stop)
-        stop_nessus
-        ;;
+                -sp|--stop)
+                stop_nessus
+                ;;
 
-        -h|--help)
-        show_options
-        ;;
-        *)
-            raise_error "Unknown argument: ${argument}"
-            ;;
-        esac
-    
-    # Second argument (if given)
-    case $arg2 in
-        -f|--firefox)
-        start_firefox
-        ;;
+                -h|--help)
+                show_options
+                ;;
 
-        -c|--chrome)
-        start_chrome
-        ;;
-        *)
-            raise_error "Unknown argument: ${argument}"
-            ;;
-    esac
+                -f|--firefox)
+                start_firefox
+                ;;
+
+                -c|--chrome)
+                start_chrome
+                ;;
+             *)
+                raise_error "Unknown argument: ${argument}"
+                ;;
+             esac
+        done
 fi
-# if [$1 == ""] ; then
-#      echo "Insufficient arguments, choose -start or -stop"
-#     exit 1
-#         ;;
-#     -start ) echo "Initializing Nessus..."
-#             sudo /etc/init.d/nessusd start || echo "nessusd not found, do you have it installed?" 
-#             sleep 4 
-#             firefox -new-tab "https://$HOSTNAME:8834"
-
-#         ;;
-#     -stop ) echo "Shutting down Nessus..."
-#             sudo /etc/init.d/nessusd stop 
-#         ;;
-#     * ) echo "You did not enter a number between 1 and 3."
-# esac
-
